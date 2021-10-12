4d63cdc707628d3bdbe91434cc16db290375d19e
dBraga <douglasabraga@outlook.com>
2021-10-12 17:30:07 -0300

    adiciona relogio analogico

--- script.js
+++ script.js
@@ -5,11 +5,20 @@ let hElement = document.querySelector('.p_h')
 
 const updateClock = () => {
     const now = new Date()
-    const hour = now.getHours()
-    const minute = now.getMinutes()
-    const second = now.getSeconds()
+    const [hour, minute, second] = [now.getHours(), now.getMinutes(), now.getSeconds()]
 
-    digitalElement.innerHTML = `${hour}:${minute}:${second}`
+    digitalElement.innerHTML = `${fixToZero(hour)}:${fixToZero(minute)}:${fixToZero(second)}`
+
+    let sDeg = ((360 / 60) * second) - 90
+    let mDeg = ((360 / 60) * minute) - 90
+    let hDeg = ((360 / 12) * hour) - 90
+
+    sElement.style.transform = `rotate(${sDeg}deg)`
+    mElement.style.transform = `rotate(${mDeg}deg)`
+    hElement.style.transform = `rotate(${hDeg}deg)`
 }
 
+const fixToZero = (number) => number < 10 ? `0${number}` : number
+
 setInterval(updateClock, 1000)
+updateClock()
